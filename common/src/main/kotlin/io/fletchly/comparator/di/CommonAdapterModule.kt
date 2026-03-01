62f9c7cc1a673ed14bccb063d990ee24bdf7e920
fletchly <owenfletchfletchly@gmail.com>
2026-02-28 19:52:10 -0700

    add Koin dependency injection setup to common module and define `commonAdapterModule` for adapter bindings

--- common/src/main/kotlin/io/fletchly/comparator/di/CommonAdapterModule.kt
+++ common/src/main/kotlin/io/fletchly/comparator/di/CommonAdapterModule.kt
@@ -16,23 +16,20 @@
  * limitations under the License.
  */
 
-plugins {
-    id("buildsrc.convention.kotlin-jvm")
-    id("buildsrc.convention.dokka-convention")
-    alias(libs.plugins.kotlinPluginSerialization)
-}
+package io.fletchly.comparator.di
 
-dependencies {
-    implementation(project(":core"))
-    implementation(libs.kotlinx.coroutines)
-    implementation(libs.kotlinx.serialization)
-    implementation(libs.bundles.ktor.client)
-    implementation("io.ktor:ktor-client-cio-jvm:3.4.0")
-    implementation(platform(libs.koin.bom))
-    implementation(libs.koin.core)
+import io.fletchly.comparator.adapter.ollama.OllamaAIProvider
+import io.fletchly.comparator.adapter.persistence.InMemoryContextStore
+import io.fletchly.comparator.adapter.tool.web.WebSearchTool
+import io.fletchly.comparator.model.tool.ToolDefinition
+import io.fletchly.comparator.port.out.AIPort
+import io.fletchly.comparator.port.out.ContextPort
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
-    testImplementation(kotlin("test"))
-    testImplementation(libs.ktor.client.mock)
-    testImplementation(libs.mockk)
-    testImplementation(libs.kotlinx.coroutines.test)
+val commonAdapterModule = module {
+    singleOf(::OllamaAIProvider) bind AIPort::class
+    singleOf(::InMemoryContextStore) bind ContextPort::class
+    singleOf(::WebSearchTool) bind ToolDefinition::class
 }
