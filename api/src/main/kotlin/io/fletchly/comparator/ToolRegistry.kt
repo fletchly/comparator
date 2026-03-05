b78de969361522de72e927de6068a4393e05396e
fletchly <owenfletchfletchly@gmail.com>
2026-03-04 19:33:17 -0700

    refactor: remove unused `Tool`-related classes and improve exception handling

--- api/src/main/kotlin/io/fletchly/comparator/ToolRegistry.kt
+++ api/src/main/kotlin/io/fletchly/comparator/ToolRegistry.kt
@@ -21,16 +21,12 @@ package io.fletchly.comparator
 import io.fletchly.comparator.model.Tool
 
 /**
- * Defines a registry for managing a collection of tools within the system.
+ * Defines a registry for managing tools that can be executed.
  *
- * This interface provides access to a list of tools that can be executed within
- * the system. The tools encapsulate specific functionality and are designed to
- * handle various tasks as part of the system's operations.
- *
- * @property tools A list of tools available in the registry. Each tool contains
- *                 its definition and execution logic.
+ * An implementation of this interface is responsible for maintaining a collection of tools and providing
+ * functionality to register new tools. Tools are expected to conform to the [Tool] structure, which includes
+ * metadata such as name, description, input parameters, and execution logic.
  */
 interface ToolRegistry {
-    val tools: List<Tool>
     fun register(vararg tools: Tool)
 }
--- api/src/main/kotlin/io/fletchly/comparator/exception/ToolException.kt
+++ api/src/main/kotlin/io/fletchly/comparator/exception/ToolException.kt
@@ -19,13 +19,10 @@
 package io.fletchly.comparator.exception
 
 /**
- * Represents an exception that occurs during tool processing or execution.
+ * Represents a recoverable, user-facing error encountered during the execution of a tool.
  *
- * This exception is used to signify errors that happen when a tool is interacting
- * with its inputs, outputs, or defined behavior. It serves as a general exception
- * for tool-related runtime issues.
- *
- * @constructor Creates a [ToolException] with a specific error message.
- * @param message A detailed message describing the reason for the exception.
+ * @constructor Creates a [ToolException] with a specified error message and an optional cause.
+ * @param message A message describing the error encountered.
+ * @param cause The underlying cause of the error, if any.
  */
-class ToolException(override val message: String) : Exception(message)
+class ToolException(override val message: String, override val cause: Throwable?) : Exception()
